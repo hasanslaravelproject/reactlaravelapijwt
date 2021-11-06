5ae7844fd56dbaff78c046554160a375d91dc402
hasanslaravelproject <hasanslaravelproject@gmail.com>
2021-11-06 15:21:52 +0100

    first commit2

--- routes/api.php
+++ routes/api.php
@@ -10,16 +10,7 @@ use App\Http\Controllers\Api\ReportController;
 use App\Http\Controllers\Api\PermissionController;
 use App\Http\Controllers\Api\ClientReportsController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
 
 Route::post('/login', [AuthController::class, 'login'])->name('api.login');
 
